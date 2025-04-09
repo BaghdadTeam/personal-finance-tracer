af8622fe4e1186f85ebd77f26e3fd657e813c824
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 18:47:16 +0200

    enhanced DataSource interface with detailed documentation for transaction methods

--- app/src/main/java/com/personalfinancetracer/repository/DataSource.kt
+++ app/src/main/java/com/personalfinancetracer/repository/DataSource.kt
@@ -2,9 +2,26 @@ package com.personalfinancetracer.repository
 
 interface DataSource {
 
-    // TODO: Import the Transaction class from the data module
-    fun <T>saveTransaction(transaction: T): Boolean
-    fun <T>saveTransactions(transactions: List<T>): Boolean
+    /**
+     * Saves a single transaction.
+     *
+     * @param transaction The transaction to be saved.
+     * @return `true` if the transaction was saved successfully, `false` otherwise.
+     */
+    fun <T> saveTransaction(transaction: T): Boolean
 
-    fun <T>loadTransactions(): List<T>
+    /**
+     * Saves a list of transactions.
+     *
+     * @param transactions The list of transactions to be saved.
+     * @return `true` if all transactions were saved successfully, `false` otherwise.
+     */
+    fun <T> saveTransactions(transactions: List<T>): Boolean
+
+    /**
+     * Loads all transactions.
+     *
+     * @return A list of transactions.
+     */
+    fun <T> loadTransactions(): List<T>
 }
