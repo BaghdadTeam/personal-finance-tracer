1f0dcf7a6109f30067c1e28569755aa17b134b7d
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 18:41:24 +0200

    added TransactionHolder to manage transactions in memory and file storage

---
+++ app/src/main/java/com/personalfinancetracer/data/storage/TransactionHolder.kt
@@ -0,0 +1,45 @@
+package com.personalfinancetracer.data.storage
+
+import com.personalfinancetracer.repository.DataSource
+
+object TransactionHolder {
+    val transactions: MutableList<Any> = mutableListOf()
+
+    private val fileTransactionStorage: DataSource = FileTransactionStorage()
+    private val memoryTransactionStorage: DataSource = InMemoryTransactionStorage()
+
+/**
+ * Loads transactions from file storage into memory storage.
+ *
+ * This method clears the current list of transactions, attempts to save
+ * the transactions loaded from file storage into memory storage, and if
+ * successful, adds the loaded transactions to the in-memory list.
+ */
+        fun loadTransactions() {
+            transactions.clear()
+            val state = memoryTransactionStorage.saveTransactions<Any>(fileTransactionStorage.loadTransactions())
+            if (state) {
+                println("Transactions have been loaded from file to memory")
+                transactions.addAll(fileTransactionStorage.loadTransactions())
+            }
+        }
+
+/**
+ * Saves a transaction to both memory and file storage.
+ *
+ * @param transaction The transaction object to be saved.
+ * @return `true` if the transaction was successfully saved to both memory and file storage,
+ *         `false` otherwise.
+ */
+    fun saveTransaction(transaction: Any): Boolean {
+        val memoryState = memoryTransactionStorage.saveTransaction(transaction)
+        val fileState = fileTransactionStorage.saveTransaction(transaction)
+        return if (memoryState && fileState) {
+            println("Transaction has been saved to file and memory")
+            true
+        } else {
+            println("Sorry there were an error while saving your transaction to the file, please try again later")
+            false
+        }
+    }
+}
