486560e186a3462aefdbab0b2f09ae37cb10768b
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 18:23:46 +0200

    implemented the functinality for InMemoryTransactionStorage

--- app/src/main/java/com/personalfinancetracer/data/storage/InMemoryTransactionStorage.kt
+++ app/src/main/java/com/personalfinancetracer/data/storage/InMemoryTransactionStorage.kt
@@ -3,15 +3,30 @@ package com.personalfinancetracer.data.storage
 import com.personalfinancetracer.repository.DataSource
 
 class InMemoryTransactionStorage: DataSource {
+
+    val transactions = mutableListOf<Any>()
+
     override fun <T> saveTransaction(transaction: T): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            this.transactions.clear()
+            this.transactions.add(transaction as Any)
+            true
+        } catch (e: Exception) {
+            println("Sorry there were an error while saving your transaction to the file, please try again later")
+            false
+        }
     }
 
     override fun <T> saveTransactions(transactions: List<T>): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            this.transactions.clear()
+            this.transactions.addAll(transactions as List<Any>)
+            true
+        } catch (e: Exception) {
+            println("Sorry there were an error while saving your transactions to the file, please try again later")
+            false
+        }
     }
 
-    override fun <T> loadTransactions(): List<T> {
-        TODO("Not yet implemented")
-    }
+    override fun <T> loadTransactions(): List<T> = transactions as List<T>
 }
