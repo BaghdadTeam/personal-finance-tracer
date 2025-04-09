14c428735ca3397e26c41a02691695f203f883ed
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 16:04:17 +0200

    created the base code for serialize and deserialize for transaction object

--- app/src/main/java/com/personalfinancetracer/utils/JsonUtil.kt
+++ app/src/main/java/com/personalfinancetracer/utils/JsonUtil.kt
@@ -1,4 +1,20 @@
 package com.personalfinancetracer.utils
 
+import org.json.JSONObject
+
 object JsonUtil {
+
+    fun <T>serializeTransaction(transaction: T): String{
+        return JSONObject().apply {
+            // Assuming T has a method to convert itself to a JSONObject
+            // This is a placeholder, you need to implement the actual serialization logic
+            // put("transaction", transaction.toJson())
+        }.toString()
+    }
+
+    fun <T>deserializeTransaction(jsonString: String): String {
+        val obj = JSONObject(jsonString)
+        TODO("Change the return type to Transaction and implement the deserialization logic")
+        return obj.toString()
+    }
 }
