5cc9aa0fb76497aeb8c60aa90be0ef055cb64423
Aboud <gh.abd.com.207@gmail.com>
2025-04-09 05:59:57 -0700

    implements file reader in category summary

--- app/src/main/java/com/personalfinancetracer/feature/report/categorySummary.kt
+++ app/src/main/java/com/personalfinancetracer/feature/report/categorySummary.kt
@@ -1,30 +1,31 @@
 package com.personalfinancetracer.feature.report
 
-import categorySummary
 
-class CategorySummary{
-     fun summary(reader : fileReader , category : String) : Map<String , Int>{
+class CategorySummary() {
+    fun summary(reader: FileReader, category: String): Map<String, Int> {
         // we may have to make it in a diffrent class
 
         var deposit = 0
         var withDraw = 0
 
-        for(row in reader){
-            if (row.category == category){
-                if (row.type == "Deposit"){
-                    deposit += row.Amount
-                }else{
+        for (row in reader.readFile()) {
+            if (row["Category"] == category) {
+                if (row["type"] == "Deposit") {
+                    deposit += row["Amount"].toString().toInt()
+                } else {
 
-                    withDraw += row.Amount
+                    withDraw += row["Amount"].toString().toInt()
                 }
             }
         }
 
-        return mapOf("Deposit" to deposit , "WithDraw" to withDraw)
+        return mapOf("Deposit" to deposit, "WithDraw" to withDraw)
 
         // idk but there should be something like deposit - withdeaw ,you may not need it
         // println("the Current for $category is $withDraw")
 
 
     }
+
+
 }
