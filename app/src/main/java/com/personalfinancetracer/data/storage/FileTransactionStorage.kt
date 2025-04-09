2e803ff30dabbd2e40fc2afb64bc0592beb118fa
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 16:21:41 +0200

    made fileTransactionStorage, InMemoryTransactionStorage inherit the data source interface

--- app/src/main/java/com/personalfinancetracer/data/storage/FileTransactionStorage.kt
+++ app/src/main/java/com/personalfinancetracer/data/storage/FileTransactionStorage.kt
@@ -1,4 +1,17 @@
 package com.personalfinancetracer.data.storage
 
-class FileTransactionStorage {
+import com.personalfinancetracer.repository.DataSource
+
+class FileTransactionStorage: DataSource {
+    override fun <T> saveTransaction(transaction: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T> saveTransactions(transactions: List<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T> loadTransactions(): List<T> {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/personalfinancetracer/data/storage/InMemoryTransactionStorage.kt
+++ app/src/main/java/com/personalfinancetracer/data/storage/InMemoryTransactionStorage.kt
@@ -1,4 +1,17 @@
 package com.personalfinancetracer.data.storage
 
-class InMemoryTransactionStorage {
+import com.personalfinancetracer.repository.DataSource
+
+class InMemoryTransactionStorage: DataSource {
+    override fun <T> saveTransaction(transaction: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T> saveTransactions(transactions: List<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T> loadTransactions(): List<T> {
+        TODO("Not yet implemented")
+    }
 }
