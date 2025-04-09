2bbb4db4af085d332facea09bc6e904102ede036
Aboud <gh.abd.com.207@gmail.com>
2025-04-09 10:48:20 -0700

    implements a total summary to interface

--- app/src/main/java/com/personalfinancetracer/feature/summary/Summary.kt
+++ app/src/main/java/com/personalfinancetracer/feature/summary/Summary.kt
@@ -5,9 +5,9 @@ import com.personalfinancetracer.feature.report.FileReader
 
 interface Summary{
     var reader : FileReader
+    fun getTotalSummary():Map<String, Int>
     fun getCategorySummary(category : String):Map<String,Int>
     fun getMonthlySummary(month : String):Map<String,Int>
     fun getCategoryMonthlySummary(category : String , month : String):Map<String,Int>
-
 }
 
