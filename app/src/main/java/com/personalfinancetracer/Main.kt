61f34d64152fd228ceca57dac0190fd633319380
sudoku <bassantelboray@gmail.com>
2025-04-09 17:48:05 +0200

    saving xml files

--- app/src/main/java/com/personalfinancetracer/Main.kt
+++ app/src/main/java/com/personalfinancetracer/Main.kt
@@ -1,7 +1,38 @@
 package com.personalfinancetracer
 
-fun main() {
+    fun main() {
+        val transactions = mutableListOf<Transaction>()
 
-    println("test");
+        while (true) {
+            println("Choose an option:")
+            println("1. Add Transaction")
+            println("2. Generate Monthly Report")
+            println("3. Exit")
 
-}
+            when (readLine()?.toIntOrNull()) {
+                1 -> {
+                    println("Enter amount:")
+                    val amount = readLine()?.toDoubleOrNull() ?: continue
+                    println("Enter type (Income/Expense):")
+                    val type = readLine() ?: continue
+                    println("Enter category:")
+                    val category = readLine() ?: continue
+                    println("Enter date (YYYY-MM-DD):")
+                    val date = readLine() ?: continue
+
+                    val newTransaction = Transaction(
+                        id = transactions.size + 1,
+                        amount = amount,
+                        type = type,
+                        category = category,
+                        date = date
+                    )
+                    transactions.add(newTransaction)
+                }
+                2 -> generateMonthlyReport(transactions)
+                3 -> return
+                else -> println("Invalid option!")
+            }
+        }
+    }
+
