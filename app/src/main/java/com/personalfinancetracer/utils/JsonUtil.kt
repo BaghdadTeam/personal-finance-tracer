f410924431ff49dea9b23e681d9a27767a1c4349
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-09 18:48:09 +0200

    enhanced JsonUtil with serialization and deserialization methods for transactions

--- app/src/main/java/com/personalfinancetracer/utils/JsonUtil.kt
+++ app/src/main/java/com/personalfinancetracer/utils/JsonUtil.kt
@@ -5,7 +5,14 @@ import org.json.JSONObject
 
 object JsonUtil {
 
-    fun <T>serializeTransaction(transaction: T): String{
+    /**
+     * Serializes a transaction object into a JSON string.
+     *
+     * @param T The type of the transaction object.
+     * @param transaction The transaction object to serialize.
+     * @return A JSON string representation of the transaction.
+     */
+    fun <T>serializeTransaction(transaction: T): String {
         return JSONObject().apply {
             // Assuming T has a method to convert itself to a JSONObject
             // This is a placeholder, you need to implement the actual serialization logic
@@ -13,12 +20,27 @@ object JsonUtil {
         }.toString()
     }
 
+    /**
+     * Deserializes a JSON string into a transaction object.
+     *
+     * @param T The type of the transaction object.
+     * @param jsonString The JSON string to deserialize.
+     * @return The deserialized transaction object.
+     * @throws NotImplementedError This method needs to be implemented.
+     */
     fun <T>deserializeTransaction(jsonString: String): T {
         val obj = JSONObject(jsonString)
         TODO("Change the return type to Transaction and implement the deserialization logic")
         return obj as T
     }
 
+    /**
+     * Serializes a list of transaction objects into a JSON string.
+     *
+     * @param T The type of the transaction objects.
+     * @param transactions The list of transaction objects to serialize.
+     * @return A JSON string representation of the list of transactions.
+     */
     fun <T>serializeTransactionList(transactions: List<T>): String {
         val jsonArray = JSONArray()
         transactions.forEach { transaction ->
@@ -27,6 +49,14 @@ object JsonUtil {
         return jsonArray.toString()
     }
 
+    /**
+     * Deserializes a JSON string into a list of transaction objects.
+     *
+     * @param T The type of the transaction objects.
+     * @param jsonString The JSON string to deserialize.
+     * @return A list of deserialized transaction objects.
+     * @throws NotImplementedError This method needs to be implemented.
+     */
     fun <T>deserializeTransactionList(jsonString: String): List<T> {
         val jsonArray = JSONArray(jsonString)
         val transactions = mutableListOf<T>()
