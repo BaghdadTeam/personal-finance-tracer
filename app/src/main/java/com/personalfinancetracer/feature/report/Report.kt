1c551367ea4faf3d5fa8c5107062a9aba6d1b0b1
Aboud <gh.abd.com.207@gmail.com>
2025-04-09 10:50:12 -0700

    implements total summary report

--- app/src/main/java/com/personalfinancetracer/feature/report/Report.kt
+++ app/src/main/java/com/personalfinancetracer/feature/report/Report.kt
@@ -2,6 +2,7 @@ import com.personalfinancetracer.feature.summary.FullSummary
 
 interface Report {
     var fullSummary: FullSummary
+    fun getTotalSummaryReport()
     fun getCategorySummaryReport(category: String)
     fun getMonthlySummaryReport(month: String)
     fun getCategoryMonthlySummaryReport(category: String, month: String)
