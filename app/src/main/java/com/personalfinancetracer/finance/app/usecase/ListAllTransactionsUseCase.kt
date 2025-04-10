c8b7268c5ee96e7f9fe52a8e1b02b369d8326393
haider <haideraljawasim@gmail.com>
2025-04-10 12:55:37 +0300

    implement the logic of listing all transactions

--- app/src/main/java/com/personalfinancetracer/finance/app/usecase/ListAllTransactionsUseCase.kt
+++ app/src/main/java/com/personalfinancetracer/finance/app/usecase/ListAllTransactionsUseCase.kt
@@ -1,11 +1,30 @@
 package com.personalfinancetracer.finance.app.usecase
 
 import com.personalfinancetracer.finance.app.Command
+import com.personalfinancetracer.models.Category
+import com.personalfinancetracer.models.Transaction
+import com.personalfinancetracer.models.TransactionType
 
-class ListAllTransactionsUseCase:Command
-{
+class ListAllTransactionsUseCase:Command {
+
+    // After merge the store to storage feature we will use actual transactions file
+    private val listOfTransactions = listOf(
+        Transaction(category = Category.FOOD, type = TransactionType.EXPENSE, amount = 2000.0),
+        Transaction(category = Category.OTHER, type = TransactionType.EXPENSE, amount = 10000.0),
+        Transaction(category = Category.TRANSPORT, type = TransactionType.EXPENSE, amount = 25000.0),
+        Transaction(category = Category.SALARY, type = TransactionType.INCOME, amount = 200000.0)
+    )
+
     override fun execute() {
-        TODO("Not yet implemented")
-    }
+        println("List of all transactions")
+        if (listOfTransactions.isEmpty()) println("No transactions found")
+        else
+            listOfTransactions.forEachIndexed { index, transaction ->
+                println("$index - ${transaction.id} | ${transaction.category} | ${transaction.type} | " +
+                        "${transaction.amount} ${transaction.date}")
+            }
 
+
+
+    }
 }
