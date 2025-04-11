068e8f5c82b014b092b56d8b892c776f9c6178b8
Aboud <gh.abd.com.207@gmail.com>
2025-04-11 13:21:41 -0700

    clean directory delete no needed files

--- app/src/main/java/com/personalfinancetracer/TransactionRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.personalfinancetracer
-
-interface TransactionRepository {
-    fun addTransaction(transaction: Transaction)
-    fun getAllTransactions(): List<Transaction>
-    fun deleteTransaction(id: Int)
-}
