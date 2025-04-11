777d8b40ac6bb4772ae37b5f2bd447c9ba9f3c39
Aboud <gh.abd.com.207@gmail.com>
2025-04-11 07:06:35 -0700

    summary report moved and rename to TransactionSummaryServices.kt

--- app/src/main/java/com/personalfinancetracer/finance/app/services/TransactionSummaryServices.kt
+++ app/src/main/java/com/personalfinancetracer/finance/app/services/TransactionSummaryServices.kt
@@ -1,26 +1,25 @@
-package com.personalfinancetracer.summaryReport
+package com.personalfinancetracer.finance.app.services
 
 import com.personalfinancetracer.datasource.FileTransactionStorage
 import com.personalfinancetracer.models.TransactionType
 import com.personalfinancetracer.utils.isDateInRange
 
 
-class TransactionSummary() : SingleUserTransactionSummary {
-    private val fileTransactionStorage =  FileTransactionStorage()
-    private val listTransaction = fileTransactionStorage.getAllTransactions()
+class TransactionSummaryServices(private val storage : FileTransactionStorage) : SingleUserTransactionSummaryServices {
 
     override fun getBalanceReport(): Map<String, Double> {
         val totalIncome =
-            listTransaction.filter { it.type == TransactionType.INCOME }.sumOf { it.amount }
+            storage.getAllTransactions().filter { it.type == TransactionType.INCOME }.sumOf { it.amount }
         val totalExpense =
-            listTransaction.filter { it.type == TransactionType.EXPENSE }.sumOf { it.amount }
+            storage.getAllTransactions().filter { it.type == TransactionType.EXPENSE }.sumOf { it.amount }
 
         val balance = totalIncome - totalExpense
         return mapOf("Income" to totalIncome, "Expense" to totalExpense , "Balance" to balance)
     }
 
     override fun getMonthlySummary(month: String): Map<String, Double> {
-        val filteredTransactions = listTransaction.filter { isDateInRange(it.date, month) }
+
+        val filteredTransactions = storage.getAllTransactions().filter { isDateInRange(it.date, month) }
         val totalIncome =
             filteredTransactions.filter { it.type == TransactionType.INCOME }.sumOf { it.amount }
         val totalExpense =
