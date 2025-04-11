6da25548cc8c4110a247f1e3ddfcd8ffb7636a40
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-11 14:01:15 +0200

    removed confilted imports from main.kt

--- app/src/main/java/com/personalfinancetracer/Main.kt
+++ app/src/main/java/com/personalfinancetracer/Main.kt
@@ -1,20 +1,7 @@
 package com.personalfinancetracer
 
-import com.personalfinancetracer.datasource.FileTransactionStorage
-import com.personalfinancetracer.models.Category
-import com.personalfinancetracer.models.Transaction
-import com.personalfinancetracer.models.TransactionType
-import java.util.Date
 
 fun main() {
 
-    FileTransactionStorage().saveTransaction(
-        Transaction(
-            date = Date(),
-            category = Category.FOOD,
-            type = TransactionType.EXPENSE,
-            amount = 49.99
-        )
-    )
 
 }
